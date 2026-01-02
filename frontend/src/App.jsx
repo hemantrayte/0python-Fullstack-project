661ba1030e7922da4ae092e049347027a2788b74
hemant <hemantrayte25@gmail.com>
2026-01-02 16:55:30 +0530

    clear app jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import Navbar from './components/Navbar'
 // import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <> 
-    <Navbar />
-      <h1>count = {count}</h1>
-      <h1>React + Django</h1>
-      <button onClick={() => setCount(count + 1)}>+</button>
+    <>
+    <h1>hemant rayte</h1>
     </>
   )
 }
