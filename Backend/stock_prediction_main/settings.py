5505b96b0fdbf40b4f5a6e20f538b2ef75ed3ea3
hemant <hemantrayte25@gmail.com>
2026-01-03 10:09:19 +0530

    add .gitignore file and update setting file

--- Backend/stock_prediction_main/settings.py
+++ Backend/stock_prediction_main/settings.py
@@ -24,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
-django-insecure-#a*=cd#3=g)y-kracl!30ox(jv)$2n@!o_hbw@#ppo4utb56w@
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False , cast=bool)
 
 ALLOWED_HOSTS = []
 
