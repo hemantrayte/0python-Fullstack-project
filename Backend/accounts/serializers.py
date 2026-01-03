0f3453170f79d364cbc65676448e60e84e9aa72c
hemant <hemantrayte25@gmail.com>
2026-01-03 13:06:39 +0530

    add user serializer

---
+++ Backend/accounts/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def create(self, validated_data):
+        # User.objects.create = save the password in a plain text
+        # User.objects.create_user = automatically hash the password
+        user = User.objects.create_user(
+            validated_data['username'],
+            validated_data['email'],
+            validated_data['password']
+        )
