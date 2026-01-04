ef1c794d3255e704044a2ff4d4c4f3f6c2d16aa8
hemant <hemantrayte25@gmail.com>
2026-01-04 16:18:42 +0530

    complete Header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,21 +1,42 @@
-import React from 'react'
+import {useContext} from 'react'
 import Button from './Button'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../context/AuthProvider'
+// import { AuthContext } from '../AuthProvider'
 
 const Header = () => {
+  const {isLoggedIn, setIsLoggedIn} = useContext(AuthContext)
+  const navigate = useNavigate();
+
+  const handleLogout = () =>{
+    localStorage.removeItem('accessToken')
+    localStorage.removeItem('refreshToken')
+    setIsLoggedIn(false)
+    console.log('Logged out');
+    navigate('/login')
+  }
   return (
     <>
-       <nav className='navbar container pt-3 pb-3 align-item-start'>
-        <Link to={'/'} className='navbar-brand text-light' href="">
-          Stock Prediction Portal
-        </Link>
+        <nav className='navbar container pt-3 pb-3 align-items-start'>
+            <Link className='navbar-brand text-light' to="/">Stock Prediction Portal</Link>
 
-        <div>
-          <Button text='Login' class="btn-outline-info" url="/login"/>
-          &nbsp:
-          <Button text='Register' class="btn-info" url="/register"/>
-        </div>
-      </nav>
+            <div>
+              {isLoggedIn ? (
+                <>
+                {/* <Button text='Dashboard' class="btn-info" url="/dashboard" />
+                &nbsp; */}
+                <button className='btn btn-danger' onClick={handleLogout}>Logout</button>
+                </>
+              ) : (
+                <>
+                <Button text='Login' class="btn-outline-info" url="/login" />
+                &nbsp;
+                <Button text='Register' class="btn-info" url="/register" />
+                </>
+              )}
+                
+            </div>
+        </nav>
     </>
   )
 }
