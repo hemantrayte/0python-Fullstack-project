12f7a14c4dd6373700226fb457eda105bca9280f
hemant <hemantrayte25@gmail.com>
2026-01-04 10:33:29 +0530

    install react router dom and set some routing

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <h1 className='text-white container'>Login</h1>
+    </div>
+  )
+}
+
+export default Login
---
+++ frontend/src/components/Register.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <>
+     <div className='container'>
+       <div className="row">
+        <div className="col-md-6">
+          <h3 className="text-light text-center">
+            Create an Account
+          </h3>
+        </div>
+       </div>
+     </div>
+    </>
+  )
+}
+
+export default Register
