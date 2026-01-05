df7ca1d0468fb3eaf24542f75b900d46f476771f
hemant <hemantrayte25@gmail.com>
2026-01-05 11:57:24 +0530

    Public Route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Login from './components/Login'
 import AuthProvider from './context/AuthProvider'
 import Dashboard from './components/dashboard/Dashboard'
 import PrivateRoute from './PrivateRoute'
+import PublicRoute from './PublicRoute'
 
 function App() {
 
@@ -18,8 +19,8 @@ function App() {
     <Header/>
        <Routes>
         <Route path='/' element={<Main />} />
-        <Route path='/register' element={<Register />}/>
-        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<PublicRoute><Register /></PublicRoute>}/>
+        <Route path='/login' element={<PublicRoute><Login /></PublicRoute>} />
         <Route path='/dashboard' element={<PrivateRoute><Dashboard /></PrivateRoute>} />
        </Routes>
     <Footer />
